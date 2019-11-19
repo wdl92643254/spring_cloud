8b32ab960f09825e851edbee48e4e976178090e6
Administrator <932634228@qq.com>
2019-11-19 22:34:12 +0800

    配置中心

---
+++ configServer/src/main/java/cn/qh/example/springcloud_config/SpringcloudConfigApplication.java
@@ -0,0 +1,13 @@
+package cn.qh.example.springcloud_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConfigApplication.class, args);
+    }
+
+}
---
+++ configServer/src/test/java/cn/qh/example/springcloud_config/SpringcloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.qh.example.springcloud_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
